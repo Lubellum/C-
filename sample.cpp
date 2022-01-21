f78f69510cd672c8ec7d98a2604b473daa773330
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-21 19:51:18 +0900

    ポインタと構造体
    ポインタを指定する場合は、指定先の構造体と同一のデータ型を指定する必要がある。
    (*p).は省略してp->と記述出来る。

--- sample.cpp
+++ sample.cpp
@@ -1,17 +1,16 @@
 #include <stdio.h>
-
-void keisan(int n1, int n2, int *tasu, int *hiku){
-    *tasu = n1 + n2;
-    *hiku = n1 - n2;
-}
+struct Fruits{
+    char name[128];
+    int price;
+    int zaiko;
+};
 
 int main (void){
-    int num1 = 30;
-    int num2 = 10;
-    int tasizan = 0;
-    int hikizan = 0;
-    keisan(num1, num2, &tasizan, &hikizan);
-    printf("足し算の結果:%D\n", tasizan);
-    printf("引き算の結果:%D\n", hikizan);
+    struct Fruits store = {"apple", 150, 50};
+    struct Fruits *p;
+    p = &store;
+    printf("名前：%s\n", p->name);
+    printf("金額：%d\n", p->price);
+    printf("在庫：%d\n", p->zaiko);
     return 0;
 }
