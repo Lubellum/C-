88d791c923f34a553a03d8378c74ce7a42ce199e
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-21 10:58:14 +0900

    変数の初期化

--- sample.cpp
+++ sample.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <stdio.h>
 using namespace std;
 
 int main (){
-    int r;
-    r = 3;
-    cout << r * r * 3.14 << "\n";
-    cout << 3 * 3 * 3.14 << "\n";
+    int value = 15;
+    cout << value << "\n";
     return 0;
 }
