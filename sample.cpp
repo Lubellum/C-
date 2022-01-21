7c11d5ef6bf30793a58d1cab1d0fe07c16f8bf53
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-21 15:37:32 +0900

    配列(２)
    宣言と代入を同時に行う

--- sample.cpp
+++ sample.cpp
@@ -2,12 +2,8 @@
 using namespace std;
 
 int main (){
-    int array[3];
-    array[0] = 10; 
-    array[1] = 20;
-    array[2] = 30;
+    int array[3] = {10, 20, 30};
     cout << array[0] << "\n";
     cout << array[1] << "\n";
     cout << array[2] << "\n";
-    return 0;
 }
