1b66fbdc24bdec9998d1aac81d32b53cd21abf67
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-21 20:28:40 +0900

    構造体を用いて複数の引数を同時に渡す

--- sample.cpp
+++ sample.cpp
@@ -6,13 +6,16 @@ struct Fruits{
     int banana;
 };
 
+void price (struct Fruits store1);
+
 int main (void){
     struct Fruits store1 = {100, 200, 300};
-    struct Fruits store2 = {200, 400, 500};
-    struct Fruits store3;
-    store3 = store1;
-    printf("りんごの金額：%d\n", store3.apple);
-    printf("オレンジの金額：%d\n", store3.orange);
-    printf("バナナの金額：%d\n", store3.banana);
+    price(store1);
     return 0;
+}
+
+void price (struct Fruits store1){
+    printf("りんごの金額：%d\n", store1.apple);
+    printf("オレンジの金額：%d\n", store1.orange);
+    printf("バナナの金額：%d\n", store1.banana);
 }
