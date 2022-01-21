8a171362c455e8a8922484348d1df27303c7fff7
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-21 18:25:20 +0900

    ポインタを用いて複数の戻り値を扱う

--- sample.cpp
+++ sample.cpp
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-void keisan(int *x){
-    *x = 25;
+void keisan(int n1, int n2, int *tasu, int *hiku){
+    *tasu = n1 + n2;
+    *hiku = n1 - n2;
 }
 
 int main (void){
-    int apple = 10;
-    printf("1回目:%d\n", apple);
-
-    keisan(&apple);
-
-    printf("書き換えました\n");
-    printf("2回目：%d\n", apple);
+    int num1 = 30;
+    int num2 = 10;
+    int tasizan = 0;
+    int hikizan = 0;
+    keisan(num1, num2, &tasizan, &hikizan);
+    printf("足し算の結果:%D\n", tasizan);
+    printf("引き算の結果:%D\n", hikizan);
+    return 0;
 }
