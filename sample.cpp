1d62d51b7f449796ec6db7106b1ebbaf247b7807
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-22 18:58:10 +0900

    ヘッダーファイルを外部で用意し、それを取り込む
    クラスの詳細を記述したファイルを別個に用意してそのファイルを読み込めばメインファイルの記述量を減らせて可読性が向上する。
    
    メンバ関数もヘッダーファイルに記述することはできるが、あまり一般的ではない。あくまでクラスの中身のみヘッダーファイルに記述する。

--- sample.cpp
+++ sample.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
+#include "sample.h"
 using namespace std;
-
-class Student{
-    public:
-        int num;
-        int year;
-        void show();
-};
 
 void Student::show(){
     cout << "番号：" << num << "\n";
-    cout << "年齢：" << year <<"\n";
 }
 
 int main (){
     Student Tarou;
-    Tarou.num = 10;
-    Tarou.year = 12;
+    Tarou.num = 24;
     Tarou.show();
     return 0;
 }
