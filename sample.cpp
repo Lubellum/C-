a8bc069da8dbd9a73f9dc1e0809256fcb40d9da5
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-23 17:32:38 +0900

    コンストラクタのオーバーロード
    オーバーロードは、同じコンストラクタでも引数が違えば複数定義出来ること。
    
    引数がある場合のパターンとない場合のパターンを用意するなど出来る。

--- sample.cpp
+++ sample.cpp
@@ -4,15 +4,21 @@ using namespace std;
 class NewClass{
     public:
         int num;
+        NewClass();
         NewClass(int x);
 };
 
+NewClass::NewClass(){
+    cout << "番号が入力されていません\n";
+}
+
 NewClass::NewClass(int x){
     num = x;
+    cout << "番号：" << num << "\n";
 }
 
 int main (){
-    NewClass obj(100);
-    cout << obj.num << "\n";
+    NewClass obj(14);
+    NewClass obj2;
     return 0;
 }
