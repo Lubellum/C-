0c39b764546cf0c9941db356883c1332994a79ac
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-24 19:01:53 +0900

    継承

--- sample.cpp
+++ sample.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 using namespace std;
 
-class NewClass{
+class OldClass{
     public:
-        NewClass();
-        NewClass(const NewClass &obj);
+        int num;
+        void show();
 };
 
-NewClass::NewClass(){
-    cout << "コンストラクタ\n";
+void OldClass::show(){
+    cout << num << "\n";
 }
 
-NewClass::NewClass(const NewClass &obj){
-    cout << "コピーコンストラクタ\n";
-}
+class NewClass : public OldClass{
+    public:
+        int num2;
+};
 
 int main (){
-    NewClass apple;
-    NewClass orange = apple;
+    NewClass obj;
+    obj.num = 10;
+    obj.num2 = 20;
+    obj.show();
+    cout << obj.num2 << "\n";
     return 0;
  }
