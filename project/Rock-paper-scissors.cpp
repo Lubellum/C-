95f9b5b622a933da80e35c40a6e3c8aad5ef5219
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-28 17:10:21 +0900

    処理の関数化

--- project/Rock-paper-scissors.cpp
+++ project/Rock-paper-scissors.cpp
@@ -5,80 +5,87 @@
 #include <time.h>
 #include <unistd.h>
 
-int main (void){
-    int Player_Choice = 0;
-    int Enemy_Choice;
-    bool End = true;
+int Player_Choice = 0;
+int Enemy_Choice;
+bool End = true;
 
-    while (End == true){
+void Draw(){
+  srand((unsigned)time(NULL));
 
-        srand((unsigned)time(NULL));
-        system("reset");
+  Enemy_Choice = rand() %3 + 1;
 
-        printf("出したい数字を入力してください\n"
-        "1 => グー\n"
-        "2 => チョキ\n"
-        "3 => パー\n"
-        "4 => 終了\n");
+  switch(Player_Choice){
+    case 1:
+      if(Enemy_Choice == 1){
+        printf("Enemyはグーを出した\n");
+        printf("あいこでした\n");
+      }
+      else if (Enemy_Choice == 2){
+        printf("Enemyはチョキを出した\n");
+        printf("勝ちました\n");
+      }
+      else {
+        printf("Enemyはパーを出した\n");
+        printf("負けました\n");
+      }
+      break;
 
-        scanf("%d", &Player_Choice);
+    case 2:
+      if(Enemy_Choice == 1){
+        printf("Enemyはグーを出した\n");
+        printf("負けました\n");
+      }
+      else if (Enemy_Choice == 2){
+        printf("Enemyはチョキを出した\n");
+        printf("あいこでした\n");
+      }
+      else {
+        printf("Enemyはパーを出した\n");
+        printf("勝ちました\n");
+      }
+      break;
 
-        rewind(stdin);
+    case 3:
+      if(Enemy_Choice == 1){
+        printf("Enemyはグーを出した\n");
+        printf("勝ちました\n");
+      }
+      else if (Enemy_Choice == 2){
+        printf("Enemyはチョキを出した\n");
+        printf("負けました\n");
+      }
+      else {
+        printf("Enemyはパーを出した\n");
+        printf("あいこでした\n");
+      }
+      break;
 
-        Enemy_Choice = rand() %3 + 1;
-
-        switch(Player_Choice){
-          case 1:
-            if(Enemy_Choice == 1){
-              printf("Enemyはグーを出した\n");
-              printf("あいこでした\n");
-            }
-            else if (Enemy_Choice == 2){
-              printf("Enemyはチョキを出した\n");
-              printf("勝ちました\n");
-            }
-            else {
-              printf("Enemyはパーを出した\n");
-              printf("負けました\n");
-            }
-            break;
-
-          case 2:
-            if(Enemy_Choice == 1){
-              printf("Enemyはグーを出した\n");
-              printf("負けました\n");
-            }
-            else if (Enemy_Choice == 2){
-              printf("Enemyはチョキを出した\n");
-              printf("あいこでした\n");
-            }
-            else {
-              printf("Enemyはパーを出した\n");
-              printf("勝ちました\n");
-            }
-            break;
-
-          case 3:
-            if(Enemy_Choice == 1){
-              printf("Enemyはグーを出した\n");
-              printf("勝ちました\n");
-            }
-            else if (Enemy_Choice == 2){
-              printf("Enemyはチョキを出した\n");
-              printf("負けました\n");
-            }
-            else {
-              printf("Enemyはパーを出した\n");
-              printf("あいこでした\n");
-            }
-            break;
-
-          case 4:
-            End = false;
-            break;
-        }
-        if(Player_Choice != 4){
-          sleep(2);
-        }
+    case 4:
+      End = false;
+      break;
+      }
+    if(Player_Choice != 4){
+      sleep(2);
     }
+}
+
+void UpData(){
+  system("reset");
+
+    printf("出したい数字を入力してください\n"
+    "1 => グー\n"
+    "2 => チョキ\n"
+    "3 => パー\n"
+    "4 => 終了\n");
+
+    scanf("%d", &Player_Choice);
+
+    rewind(stdin);
+}
+
+int main (void){
+  while (End == true){
+    Draw();
+    UpData();
+  }
 }
