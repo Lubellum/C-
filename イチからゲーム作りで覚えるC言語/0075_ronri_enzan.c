c3eabd1040e74af20790a787dc20cd667112bbcc
Lubellum <azukyun0202@gmail.com>
2022-08-09 16:58:59 +0900

    更新

--- イチからゲーム作りで覚えるC言語/0075_ronri_enzan.c
+++ イチからゲーム作りで覚えるC言語/0075_ronri_enzan.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 
 int main() {
-  bool HpNoneFlag = true;            // 体力無しフラグ
+  bool HpNoneFlag = 1;            // 体力無しフラグ
   bool RecoveryAgentsNoFlag = false; // 回復薬なしフラグ
   bool timeoutFlag = false;          // 時間切れフラグ
 
@@ -10,14 +10,16 @@ int main() {
   bool deathFlag = HpNoneFlag && RecoveryAgentsNoFlag;
   bool seizonFlag = !deathFlag;
 
-  printf("体力無しフラグ:%d\n", HpNoneFlag);
-  printf("回復薬無しフラグ:%d\n", RecoveryAgentsNoFlag);
-  printf("時間切れフラグ:%d\n", timeoutFlag);
+  printf("     体力無しフラグ:%d\n", HpNoneFlag);
+  printf("   回復薬無しフラグ:%d\n", RecoveryAgentsNoFlag);
+  printf("     時間切れフラグ:%d\n", timeoutFlag);
   printf("ゲームオーバーフラグ:%d\n", gameOverFlag);
-  printf("死亡フラグ:%d\n", deathFlag);
-  printf("生存フラグ:%d\n", seizonFlag);
+  printf("        死亡フラグ:%d\n", deathFlag);
+  printf("        生存フラグ:%d\n", seizonFlag);
 
   bool a = 150;
   printf("bool型に整数150を代入した結果:%d\n", a);
 
+  bool flag = 3 <= 4;
+  printf("%d", flag);
 }
